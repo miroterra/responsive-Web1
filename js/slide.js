d08b38b56d74ea4bca4abdab7807d629ceb325b2
Takphantom <miroterra@naver.com>
2022-03-31 21:10:31 +0900

    슬라이드 완성

--- js/slide.js
+++ js/slide.js
@@ -1,55 +1,53 @@
 const slides = document.getElementsByClassName('slideBox'),
   dots = document.getElementsByClassName('dot');
-let slideIndex = 1;
-showSlides(slideIndex);
+
+let slideIndex = 0,
+  myVar;
+
+showSlides();
+
+function showSlides() {
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+    dots[i].className = dots[i].className.replace(' active', '');
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = 'block';
+  dots[slideIndex - 1].className += ' active';
+  myVar = setTimeout(showSlides, 4000);
+}
+
+function stopSlide() {
+  clearTimeout(myVar);
+}
 
 function plusSlides(n) {
-  autoSlide();
-  showSlides((slideIndex += n));
+  stopSlide();
+  slideIndex += n;
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+    dots[i].className = dots[i].className.replace(' active', '');
+  }
+  if (slideIndex < 1) {
+    slideIndex = slides.length;
+  }
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = 'block';
+  dots[slideIndex - 1].className += ' active';
 }
 
 function currentSlide(n) {
-  autoSlide();
-  showSlides((slideIndex = n));
-}
-
-function showSlides(n) {
-  let i;
-
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
+  stopSlide();
+  slideIndex = n;
+  for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = 'none';
     dots[i].className = dots[i].className.replace(' active', '');
   }
   slides[slideIndex - 1].style.display = 'block';
   dots[slideIndex - 1].className += ' active';
-  autoSlide();
-  function autoSlide() {
-    slideIndex++;
-    setTimeout(showSlides, 2000);
-    if (slideIndex > slides.length) {
-      slideIndex = 1;
-    }
-  }
 }
-// function autoSlide() {
-//   let j;
-//   for (j = 0; j < slides.length; j++) {
-//     slides[j].style.display = 'none';
-//   }
-//   slideIndex++;
-//   if (slideIndex > slides.length) {
-//     slideIndex = 1;
-//   }
-//   for (j = 0; j < dots.length; j++) {
-//     dots[j].className = dot[j].className.replace(' active', '');
-//   }
-//   slides[slideIndex - 1].style.display = 'block';
-//   dots[slideIndex - 1].className += ' active';
-//   setTimeout(autoSlide, 2000);
-// }
